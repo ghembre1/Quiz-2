876c680490f007edd0400926a6235b9a88f72384
Gary Hembree <gbhembree22@amarillocollege.com>
2024-08-28 12:58:27 -0500

    Added comments to the code

--- Quiz 2/Program.cs
+++ Quiz 2/Program.cs
@@ -11,16 +11,22 @@ namespace Quiz_2
     {
         static void Main(string[] args)
         {
+            // prompts user to enter price
             Console.WriteLine("Enter the item price:");
             double price = Convert.ToDouble(Console.ReadLine());
 
+            //prompts user to enter the tax
             Console.WriteLine("Enter the sales tax:");
             double tax = Convert.ToDouble(Console.ReadLine());
 
+            //prompts user to enter the discount
             Console.WriteLine("Enter promotional discount");
             double discount = Convert.ToDouble(Console.ReadLine());
 
+            //Equation to solve for the cost
             double Cost = (price * tax)  + (price * discount);
+            
+            //Writes the line for the total cost
             Console.Write("The total cost for the item is ");
             Console.Write(Cost);
 
