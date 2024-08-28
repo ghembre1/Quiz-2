f1d17444abe905f23fbec562bc1036da1ea7f6cc
Gary Hembree <gbhembree22@amarillocollege.com>
2024-08-28 12:56:39 -0500

    Add project files.

---
+++ Quiz 2/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Schema;
+
+namespace Quiz_2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter the item price:");
+            double price = Convert.ToDouble(Console.ReadLine());
+
+            Console.WriteLine("Enter the sales tax:");
+            double tax = Convert.ToDouble(Console.ReadLine());
+
+            Console.WriteLine("Enter promotional discount");
+            double discount = Convert.ToDouble(Console.ReadLine());
+
+            double Cost = (price * tax)  + (price * discount);
+            Console.Write("The total cost for the item is ");
+            Console.Write(Cost);
+
+            Console.ReadLine();
+        }
+    }
+}
